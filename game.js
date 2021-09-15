3b6055a13c8b5537cf8ace7b1041a93a31f28b9f
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-15 12:51:21 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ import { update as updateFood, draw as drawFood} from './food.js'
 
 let lastRenderTime =0
 const gameBoard = document.getElementById('game-board')
-
+let gameOver = false
 
 
 function main(currentTime){
@@ -29,6 +29,7 @@ function update(){
     //update food and snake
     updateSnake()
     updateFood()
+    checkDeath()
 }
 
 function draw(){
@@ -38,4 +39,11 @@ gameBoard.innerHTML = ''//clear our view of the snake previous body
     drawSnake(gameBoard)
     drawFood(gameBoard)
 
+}
+
+
+function checkDeath(){
+    //to end the game if outside or hit its self
+
+    gameOver = outsideGrid(getSnakeHead) || snakeIntersection()
 }
